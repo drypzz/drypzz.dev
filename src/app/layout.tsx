5e9c4187af1fa645aeff7910ccb13fcd005a2016
drypzz <fdfjonfmd@gmail.com>
2024-02-14 14:44:54 -0300

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,25 @@ export const metadata: Metadata = {
   description: 'Olá, seja bem vindo(a), esse é meu portfolio. Aqui você verá alguns de meus projetos e minhas tecnologias 🙂.',
 };
 
+type HeadProps = {
+  image: string;
+  url: string;
+  type: string;
+  color: string;
+};
+
+export const getHeads = ({image, url, type, color}: HeadProps) => {
+  return (
+    <>
+      <meta property='og:image' content={image} />
+      <meta property='og:url' content={url} />
+      <meta property='og:type' content={type} />
+      <meta property='og:color' content={color} />
+      <meta property='twitter:color' content={color} />
+    </>
+  )
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -18,9 +37,12 @@ export default function RootLayout({
     <html lang='pt-br'>
       <head>
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        <meta property='og:image' content='/images/0.jpg' />
-        <meta property='og:url' content='https://drypzz.netlify.app/' />
-        <meta property='og:type' content='website' />
+        {getHeads({
+          image: '/images/0.jpg',
+          url: 'https://drypzz.netlify.app/',
+          type: 'website',
+          color: '#037edb'
+        })}
       </head>
       <body className={inter.className}>
         <main className='gradient-bg-welcome'>
