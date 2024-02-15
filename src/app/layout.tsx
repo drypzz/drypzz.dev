f02da1e0fa2c94380846aabb6f75d3db384179e4
drypzz <fdfjonfmd@gmail.com>
2024-02-15 13:10:06 -0300

    fixed meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,22 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
+ 
+export const viewport: Viewport = {
+  themeColor: '#037edb',
+  initialScale: 1,
+  width: 'device-width',
+}
+
 export const metadata: Metadata = {
+  metadataBase: new URL('https://drypzz.netlify.app/'),
   title: 'DRYPZZ - DEV',
   applicationName: 'DRYPZZ - DEV',
   description: 'Olá, seja bem vindo(a), esse é meu portfolio. Aqui você verá alguns de meus projetos e minhas tecnologias 🙂.',
   creator: 'DRYPZZ',
-  viewport: { width: 'device-width', initialScale: 1 },
-  themeColor: '#037edb',
   authors: [{ name: 'Next.js Team', url: 'https://nextjs.org' }],
   generator: 'NextJS',
   keywords: ['DRYPZZ', 'DEV', 'portfolio', 'projetos', 'tecnologias', 'desenvolvimento', 'web', 'mobile', 'front-end', 'back-end', 'fullstack', 'programação', 'programador', 'desenvolvedor', 'webdev', 'webdeveloper', 'webdesign'],
